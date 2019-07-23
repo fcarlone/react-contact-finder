6399680ebe00c45f1148bfd43c8b8e32c73daf1b
fcarlone <frankcarlone@gmail.com>
2019-07-23 12:11:57 -0400

    Setup deployment to Heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -11,15 +12,21 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-// Add a route
-app.get("/", (req, res) => {
-  res.json({ msg: "Welcome" });
-});
-
 // Define Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
 app.use("/api/users", require("./routes/users"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  // route
+  app.get("*", (req, res) => {
+    res.senfFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server started on port: ${PORT}`);
